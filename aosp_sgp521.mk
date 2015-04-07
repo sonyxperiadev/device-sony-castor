0db0345d6b586a4aec9a5d12e5a0c0be43aa0637
David Viteri <davidteri91@gmail.com>
2015-04-07 16:28:43 +0200

    castor: add init.usb.rc to device specific
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- aosp_sgp521.mk
+++ aosp_sgp521.mk
@@ -64,7 +64,8 @@ PRODUCT_COPY_FILES += \
     device/sony/castor/rootdir/system/etc/tfa98xx/FMSpeaker_btm.eq:/system/etc/tfa98xx/FMSpeaker_btm.eq \
     device/sony/castor/rootdir/system/etc/tfa98xx/TFA9890_Receiver.config:/system/etc/tfa98xx/TFA9890_Receiver.config \
     device/sony/castor/rootdir/system/etc/tfa98xx/VoiceCallEarpice_top.preset:/system/etc/tfa98xx/VoiceCallEarpice_top.preset \
-    device/sony/castor/rootdir/system/etc/tfa98xx/VoiceCallEarpice_top.eq:/system/etc/tfa98xx/VoiceCallEarpice_top.eq
+    device/sony/castor/rootdir/system/etc/tfa98xx/VoiceCallEarpice_top.eq:/system/etc/tfa98xx/VoiceCallEarpice_top.eq \
+    device/sony/castor/rootdir/init.castor.usb.rc:root/init.castor.usb.rc
 
 PRODUCT_NAME := aosp_sgp521
 PRODUCT_DEVICE := castor
