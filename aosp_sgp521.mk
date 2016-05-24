574bd6ed68222dd5fd8c37253d69a4140f4dde23
David <Davidteri91@gmail.com>
2016-05-24 23:44:35 +0200

    Castor: Build InCallUI Signed-off-by: David Viteri <davidteri91@gmail.com>

--- aosp_sgp521.mk
+++ aosp_sgp521.mk
@@ -34,8 +34,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     nfc_nci.castor
 
-# SimToolKit
+# Telephony Packages (AOSP)
 PRODUCT_PACKAGES += \
+    InCallUI \
     Stk
 
 # Inherit from those products. Most specific first.
