ee805cd08f70f6f3c734834372ef4454e75c0a4c
David <Davidteri91@gmail.com>
2016-08-28 08:32:46 +0200

    castor: move mms_useragent per device from common
    this should be info per target and not common
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- aosp_sgp521.mk
+++ aosp_sgp521.mk
@@ -14,6 +14,9 @@
 
 TARGET_KERNEL_CONFIG := aosp_shinano_castor_defconfig
 
+DEVICE_PACKAGE_OVERLAYS += \
+    device/sony/castor/overlay
+
 # Device Specific Permissions
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
